7e0bdd5ea9e9ca3f858f4b1f0bac33b8f53165a6
Sergey Rush <sergey.rush@hotmail.com>
2018-12-10 01:26:51 +0300

    Init

---
+++ DesignPatterns/MemoryAddress.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace DesignPatterns
+{
+
+    public static class Extensions
+    {
+        private static readonly ConditionalWeakTable<object, RefId> _ids = new ConditionalWeakTable<object, RefId>();
+
+        public static Guid GetRefId<T>(this T obj) where T : class
+        {
+            if (obj == null)
+                return default(Guid);
+
+            return _ids.GetOrCreateValue(obj).Id;
+        }
+
+        private class RefId
+        {
+            public Guid Id { get; } = Guid.NewGuid();
+        }
+    }
+}
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Age = 32;
+            person.Name = "Alex";
+            
+            Guid df = person.GetRefId();
+
+            string s1 = "name";
+            string s2 = "name";
+
+            Guid us1 = s1.GetRefId();
+            Guid us2 = s2.GetRefId();
+
+        }
+
+        public class Person
+        {
+            public int Age { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
